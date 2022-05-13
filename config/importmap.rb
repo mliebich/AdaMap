f48567515b2386b0f14e5426aa8104cf9a3f50d7
Michael Liebich <michi.liebich@gmail.com>
2022-05-13 13:43:39 +0200

    adds leader-line js library via importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "leader-line", to: "https://ga.jspm.io/npm:leader-line@1.0.7/leader-line.min.js"
