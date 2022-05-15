bdd8ddadda3580fd5d4674ef846120d512a7ff33
Michael Liebich <michi.liebich@gmail.com>
2022-05-15 16:11:13 +0200

    adds join table for user and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   has_rich_text :content
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :tasks
+  has_and_belongs_to_many :goals, class_name: 'Task'
 end
