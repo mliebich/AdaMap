b3de92ae414d04936baa5b80a3b83e6100419a9f
Michael Liebich <michi.liebich@gmail.com>
2022-08-11 11:09:37 +0200

    adds css partials (@apply in application.tailwind.css) to keep things dry

--- app/formbuilder/TailwindFormBuilder.rb
+++ app/formbuilder/TailwindFormBuilder.rb
@@ -10,12 +10,12 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     super(method, merged_opts)
   end
   def submit(method, opts={})
-    default_opts = { class: "text-yellow-500 hover:text-white border border-yellow-500 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900" }
+    default_opts = { class: "btn" }
     merged_opts = default_opts.merge(opts)
     super(method, merged_opts)
   end
   def check_box(method, opts={})
-    default_opts = { class: "w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" }
+    default_opts = { class: "w-4 h-4 bg-gray-50 rounded border border-gr-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" }
     merged_opts = default_opts.merge(opts)
     super(method, merged_opts)
   end
