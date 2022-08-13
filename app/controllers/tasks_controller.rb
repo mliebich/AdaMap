ab551de7ca44683c9efd7de66589a3f1e6629f63
Michael Liebich <michi.liebich@gmail.com>
2022-08-13 11:36:37 +0200

    disable add button if goal already exists for user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,7 @@ class TasksController < ApplicationController
 
     if (session[:user_id].present?)
       @user = User.find(session[:user_id])
+      @disable_add_goal = @user.goals.exists?(@idea.id)
     else
       @user = nil
     end
