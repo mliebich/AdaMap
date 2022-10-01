aa92ba97837eebc4eae87362fce70f485d83d9d5
Michael Liebich <michi.liebich@gmail.com>
2022-10-01 11:23:51 +0200

    removes code for draggability, adds gradient to line

--- app/javascript/controllers/learningmap_controller.js
+++ app/javascript/controllers/learningmap_controller.js
@@ -9,29 +9,24 @@ export default class extends Controller {
   initialize() {
       console.log(this.idValue)
       console.log(this.preValue)
-      //console.log(this.element.innerHTML)
-      this.index = 0
     }
 
   connect(params) {
     var start = document.getElementById(this.idValue)
     var end = document.getElementById(this.preValue)
-    function fixLine() {
-      line.position();
-    }
-    new PlainDraggable(start, {onMove: fixLine});
     var line = new LeaderLine(end, start,
     {
       size: 3,
       dash: { animation: true },
       line: { path: 'fluid'},
       startSocket: 'top',
-      startPlug: 'square',
-      startPlugSize: 1.5,
-      startPlugColor: 'green',
-      endPlug: 'disc',
-      endPlugColor: 'blue',
-      endPlugSize: 2.5,
+      startPlug: 'disc',
+      startPlugSize: 3.5,
+      startPlugColor: '#0d9488',
+      endPlug: 'hand',
+      endPlugColor: '#0ea5e9',
+      endPlugSize: 1.25,
+      gradient: true
     })
   }
 }
